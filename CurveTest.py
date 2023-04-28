1f029f4c5afb825c990d4affa37ae2c1e02eb2ef
Loloppe <54846763+Loloppe@users.noreply.github.com>
2023-04-28 11:38:45 -0400

    Update CurveTest.py

--- CurveTest.py
+++ CurveTest.py
@@ -6,18 +6,25 @@ import Tech_Calculator.tech_calc as tech_calc
 import Tech_Calculator._BackendFiles.setup as setup
 import math
 
-playerTestList = [2769016623220259,76561198059961776,76561198072855418,76561198075923914,76561198255595858,76561198404774259, 
-    76561198225048252, 76561198110147969, 76561198081152434, 76561198204808809, 76561198072431907, 76561198989311828, 76561198960449289, 
-    76561199104169308, 3225556157461414, 76561198410971373, 76561198153101808, 76561197995162898, 2169974796454690, 76561198166289091, 
-    76561198285246326, 76561198802040781, 76561198110018904, 76561198044544317, 2092178757563532, 76561198311143750, 76561198157672038,
-    76561199050525271]
+playerTestList = [76561198073989976] #[3225556157461414,76561198225048252, 76561198059961776, 76561198072855418, 76561198075923914,
+                  #76561198255595858, 76561198404774259,
+                  #76561198110147969, 76561198081152434, 76561198204808809, 76561198072431907,
+                  #76561198989311828, 76561198960449289,
+                  #76561199104169308, 2769016623220259, 76561198410971373, 76561198153101808, 76561197995162898,
+                  #2169974796454690, 76561198166289091,
+                  #76561198285246326, 76561198802040781, 76561198110018904, 76561198044544317, 2092178757563532,
+                  #76561198311143750, 76561198157672038,
+                  #76561199050525271, 76561198272028078, 76561198027274310]
 
-#playerTestList = [76561199050525271]
+
+# playerTestList = [76561198027274310]
 
 def searchDiffIndex(diffNum, diffList):
     for f in range(0, len(diffList)):
         if diffList[f]['value'] == diffNum:
             return f
+
+
 def convertSpeed(listOfMods):
     speed = 1
     if 'FS' in listOfMods:
@@ -27,43 +34,47 @@ def convertSpeed(listOfMods):
     if 'SS' in listOfMods:
         speed = 0.85
     return speed
+
+
 def getKey(JSON):
     key = JSON['leaderboard']['song']['id']
     key = key.replace('x', '')
     return key
+
+
 def pointList1(acc):
     pointList = [[1.0, 7.424],
-                [0.999, 6.241],
-                [0.9975, 5.158],
-                [0.995, 4.010],
-                [0.9925, 3.241],
-                [0.99, 2.700],
-                [0.9875, 2.303],
-                [0.985, 2.007],
-                [0.9825, 1.786],
-                [0.98, 1.618],
-                [0.9775, 1.490],
-                [0.975, 1.392],
-                [0.9725, 1.315],
-                [0.97, 1.256],
-                [0.965, 1.167],
-                [0.96, 1.101],
-                [0.955, 1.047],
-                [0.95, 1.000],
-                [0.94, 0.919],
-                [0.93, 0.847],
-                [0.92, 0.786],
-                [0.91, 0.734],
-                [0.9, 0.692],
-                [0.875, 0.606],
-                [0.85, 0.537],
-                [0.825, 0.480],
-                [0.8, 0.429],
-                [0.75, 0.345],
-                [0.7, 0.286],
-                [0.65, 0.246],
-                [0.6, 0.217],
-                [0.0, 0.000]]
+                 [0.999, 6.241],
+                 [0.9975, 5.158],
+                 [0.995, 4.010],
+                 [0.9925, 3.241],
+                 [0.99, 2.700],
+                 [0.9875, 2.303],
+                 [0.985, 2.007],
+                 [0.9825, 1.786],
+                 [0.98, 1.618],
+                 [0.9775, 1.490],
+                 [0.975, 1.392],
+                 [0.9725, 1.315],
+                 [0.97, 1.256],
+                 [0.965, 1.167],
+                 [0.96, 1.101],
+                 [0.955, 1.047],
+                 [0.95, 1.000],
+                 [0.94, 0.919],
+                 [0.93, 0.847],
+                 [0.92, 0.786],
+                 [0.91, 0.734],
+                 [0.9, 0.692],
+                 [0.875, 0.606],
+                 [0.85, 0.537],
+                 [0.825, 0.480],
+                 [0.8, 0.429],
+                 [0.75, 0.345],
+                 [0.7, 0.286],
+                 [0.65, 0.246],
+                 [0.6, 0.217],
+                 [0.0, 0.000]]
     for i in range(0, len(pointList)):
         if pointList[i][0] <= acc:
             break
@@ -74,50 +85,55 @@ def pointList1(acc):
     middle_dis = (acc - pointList[i - 1][0]) / (pointList[i][0] - pointList[i - 1][0])
 
     return pointList[i - 1][1] + middle_dis * (pointList[i][1] - pointList[i - 1][1])
+
+
 def pointList2(acc):
     pointList = [[1.0, 7.424],
-        [0.999, 6.241],
-        [0.9975, 5.158],
-        [0.995, 4.010],
-        [0.9925, 3.241],
-        [0.99, 2.700],
-        [0.9875, 2.303],
-        [0.985, 2.007],
-        [0.9825, 1.786],
-        [0.98, 1.618],
-        [0.9775, 1.490],
-        [0.975, 1.392],
-        [0.9725, 1.315],
-        [0.97, 1.256],
-        [0.965, 1.167],
-        [0.96, 1.094],
-        [0.955, 1.039],
-        [0.95, 1.000],
-        [0.94, 0.931],
-        [0.93, 0.867],
-        [0.92, 0.813],
-        [0.91, 0.768],
-        [0.9, 0.729],
-        [0.875, 0.650],
-        [0.85, 0.581],
-        [0.825, 0.522],
-        [0.8, 0.473],
-        [0.75, 0.404],
-        [0.7, 0.345],
-        [0.65, 0.296],
-        [0.6, 0.256],
-        [0.0, 0.000]]
+                 [0.999, 6.241],
+                 [0.9975, 5.158],
+                 [0.995, 4.010],
+                 [0.9925, 3.241],
+                 [0.99, 2.700],
+                 [0.9875, 2.303],
+                 [0.985, 2.007],
+                 [0.9825, 1.786],
+                 [0.98, 1.618],
+                 [0.9775, 1.490],
+                 [0.975, 1.392],
+                 [0.9725, 1.315],
+                 [0.97, 1.256],
+                 [0.965, 1.167],
+                 [0.96, 1.094],
+                 [0.955, 1.039],
+                 [0.95, 1.000],
+                 [0.94, 0.931],
+                 [0.93, 0.867],
+                 [0.92, 0.813],
+                 [0.91, 0.768],
+                 [0.9, 0.729],
+                 [0.875, 0.650],
+                 [0.85, 0.581],
+                 [0.825, 0.522],
+                 [0.8, 0.473],
+                 [0.75, 0.404],
+                 [0.7, 0.345],
+                 [0.65, 0.296],
+                 [0.6, 0.256],
+                 [0.0, 0.000]]
     for i in range(0, len(pointList)):
         if pointList[i][0] <= acc:
             break
-    
+
     if i == 0:
         i = 1
-    
-    middle_dis = (acc - pointList[i-1][0]) / (pointList[i][0] - pointList[i-1][0])
 
-    return pointList[i-1][1] + middle_dis * (pointList[i][1] - pointList[i-1][1])
+    middle_dis = (acc - pointList[i - 1][0]) / (pointList[i][0] - pointList[i - 1][0])
 
+    return pointList[i - 1][1] + middle_dis * (pointList[i][1] - pointList[i - 1][1])
+
+
+def inflate(pp):
+    return (650 * math.pow(pp, 1.3)) / math.pow(650, 1.3)
 
 
 def load_Song_Stats(dataJSON, speed, key, retest=False, versionNum=-1):
@@ -142,15 +158,15 @@ def load_Song_Stats(dataJSON, speed, key, retest=False, versionNum=-1):
                 AiJSON['lackStats'] = tech_calc.mapCalculation(mapData, bpm, False, False)
                 AiJSON['versionNum'] = versionNum
                 result = s.get(
-                     f"https://bs-replays-ai.azurewebsites.net/json/{hash}/{diffNum}/time-scale/{speed}")
+                    f"https://bs-replays-ai.azurewebsites.net/bl-reweight/{hash}/Standard/{diffNum}")
                 if result.text == 'Not found':
-                     AiJSON['AIstats'] = {}
-                     AiJSON['AIstats']['balanced'] = 0
-                     AiJSON['AIstats']['expected_acc'] = 0
-                     AiJSON['AIstats']['passing_difficulty'] = 0
-                     # AiJSON['expected_acc'] = 1
+                    AiJSON['AIstats'] = {}
+                    AiJSON['AIstats']['balanced'] = 0
+                    AiJSON['AIstats']['expected_acc'] = 0
+                    AiJSON['AIstats']['passing_difficulty'] = 0
+                    # AiJSON['expected_acc'] = 1
                 else:
-                     AiJSON['AIstats'] = json.loads(result.text)
+                    AiJSON['AIstats'] = json.loads(result.text)
                 try:
                     os.mkdir(f"_AIcache/{hash}")
                 except:
@@ -161,7 +177,7 @@ def load_Song_Stats(dataJSON, speed, key, retest=False, versionNum=-1):
     except:
         print("Requesting from AI and Calculator")
         result = s.get(
-            f"https://bs-replays-ai.azurewebsites.net/json/{hash}/{diffNum}/time-scale/{speed}")
+            f"https://bs-replays-ai.azurewebsites.net/bl-reweight/{hash}/Standard/{diffNum}")
         if result.text == 'Not found':
             newStar = dataJSON['leaderboard']['song']['difficulties'][diffIndex]['stars']
             AiJSON['AIstats'] = {}
@@ -177,16 +193,15 @@ def load_Song_Stats(dataJSON, speed, key, retest=False, versionNum=-1):
         if mapData != None:
             AiJSON['lackStats'] = tech_calc.mapCalculation(mapData, bpm, False, False)
         else:
-            AiJSON['lackStats'] = {'tech': 0, 'balanced_pass_diff': 0}
+            AiJSON['lackStats'] = {'balanced_tech': 0, 'balanced_pass_diff': 0}
         try:
             os.mkdir(f"_AIcache/{hash}")
         except:
             print("Existing Folder")
         with open(f"_AIcache/{hash}/{diffNum} {speed}.json", 'w') as score_json:
             json.dump(AiJSON, score_json, indent=4)
-    
-    return AiJSON
 
+    return AiJSON
 
 
 def newPlayerStats(userID, scoreCount, retest=False, versionNum=-1):
@@ -211,40 +226,51 @@ def newPlayerStats(userID, scoreCount, retest=False, versionNum=-1):
                 if playerJSON['data'][i]['leaderboard']['difficulty']['modeName'] == 'Standard':
                     speed = convertSpeed(playerJSON['data'][i]['modifiers'].split(','))
                     songStats = load_Song_Stats(playerJSON['data'][i], speed, key, retest, versionNum)
-
-                    AIacc = songStats['AIstats']['expected_acc']
+                    modifier = playerJSON['data'][i]['modifiers']
+                    if "FS" in modifier:
+                        modifier = "FS"
+                    elif "SFS" in modifier:
+                        modifier = "SFS"
+                    elif "SS" in modifier:
+                        modifier = "SS"
+                    else:
+                        modifier = "none"
+                    AIacc = songStats['AIstats'][modifier]['AIacc']
                     playerACC = playerJSON['data'][i]['accuracy']
                     passRating = songStats['lackStats']['balanced_pass_diff']
                     tech = songStats['lackStats']['balanced_tech']
 
-                    passPP = 15.2 * math.exp(passRating ** (1 / 2.62)) - 30
+                    passPP = 15.2 * math.exp(math.pow(passRating, 1 / 2.62)) - 30
+
+                    if passPP < 0:
+                        passPP = 0
 
                     if AIacc != 0:
                         AI600Star = 15 / pointList1(AIacc + 0.0022)
                     else:
                         tinyTech = 0.0208 * tech + 1.1284  # https://www.desmos.com/calculator/yaqyyomsp9
-                        AI600Star = (-tinyTech ** (-passRating)) + 1 * 8 + 2 + 0.01 * tech * passRating
+                        AI600Star = (-math.pow(tinyTech, -passRating) + 1) * 8 + 2 + 0.01 * tech * passRating
 
                     playerTechPP = math.exp(1.9 * playerACC) * 1.08 * tech
-                    # playerTechPP = 1 / (1 + math.e**(-64 * (playerACC - 0.9))) * tech / (1 + math.e**(-16 * (tech - 0.5))) * 15 * AI600Star / max((0.3333 * passRating), 1)
                     playerAccPP = pointList2(playerACC) * AI600Star * 34
-                    # playerAccPP = curveAccMulti(balancedAcc) * 175 * (-math.e**(-passRating-0.05) + 1)
-                    playerPP = passPP + playerAccPP + playerTechPP
-                    
+                    playerPP = inflate(passPP + playerAccPP + playerTechPP)
+
                     newStats.append({})
                     newStats[-1]['name'] = playerJSON['data'][i]['leaderboard']['song']['name']
-                    newStats[-1]['diff'] = playerJSON['data'][i]['leaderboard']['song']['difficulties'][diffIndex]['difficultyName']
-                    newStats[-1]['passRating'] = passRating
-                    newStats[-1]['oldStar'] = playerJSON['data'][i]['leaderboard']['song']['difficulties'][diffIndex]['stars']
+                    newStats[-1]['diff'] = playerJSON['data'][i]['leaderboard']['song']['difficulties'][diffIndex][
+                        'difficultyName']
+                    newStats[-1]['Pass'] = passRating
+                    newStats[-1]['Acc'] = AI600Star
+                    newStats[-1]['Tech'] = tech
                     newStats[-1]['Modifiers'] = playerJSON['data'][i]['modifiers']
                     newStats[-1]['acc'] = playerACC
-                    newStats[-1]['tech'] = tech
+                    newStats[-1]['oldStar'] = playerJSON['data'][i]['leaderboard']['song']['difficulties'][diffIndex][
+                        'stars']
                     newStats[-1]['oldPP'] = playerJSON['data'][i]['pp']
                     newStats[-1]['passPP'] = passPP
                     newStats[-1]['techPP'] = playerTechPP
                     newStats[-1]['accPP'] = playerAccPP
                     newStats[-1]['playerPP'] = playerPP
-                    newStats[-1]['600PassStar'] = AI600Star
 
 
     newStats = sorted(newStats, key=lambda x: x.get('playerPP', 0), reverse=True)
@@ -259,7 +285,7 @@ def newPlayerStats(userID, scoreCount, retest=False, versionNum=-1):
         os.mkdir(str(f'{filePath}'))
         with open(f'{filePath}/dataNewPlayerPP.json', 'w') as data_json:
             json.dump(newStats, data_json, indent=4)
-    
+
     newStats = sorted(newStats, key=lambda x: x.get('passRating', 0), reverse=True)
     with open(f'{filePath}/dataStar.json', 'w') as data_json:
         json.dump(newStats, data_json, indent=4)
@@ -277,20 +303,13 @@ def newPlayerStats(userID, scoreCount, retest=False, versionNum=-1):
         json.dump(newStats, data_json, indent=4)
 
 
-
-
-
-
-
-
-
 if __name__ == "__main__":
     print("Re-test tech calculator? y/n")
     retest = input()
     if retest.lower() == 'y':
         retest = True
         try:
-            f = open('Tech_Calculator/_BackendFiles/techversion.txt', 'r') # Use This to try and find an existing file
+            f = open('Tech_Calculator/_BackendFiles/techversion.txt', 'r')  # Use This to try and find an existing file
             versionNum = int(f.read()) + 1
             f = open('Tech_Calculator/_BackendFiles/techversion.txt', 'w')
             f.write(str(versionNum))
