06f6132a2c375f6eb184ba254203578fd93327fa
Loloppe <54846763+Loloppe@users.noreply.github.com>
2023-05-05 22:01:22 -0400

    Revert beat duration stuff

--- Tech_Calculator/tech_calc.py
+++ Tech_Calculator/tech_calc.py
@@ -301,7 +301,7 @@ def handlePattern(mapSplitData: list):
                     direction = reverseCutDirection(mod(cut_direction_index[foundArrow[0]['d']] +
                                                         foundArrow[0]['a'], 360))
                     for i in range(mapSplitData.index(foundArrow[0]) - 1, n, -1):
-                        if mapSplitData[i + 1]['b'] - mapSplitData[i]['b'] >= findBPM(mapSplitData[n]['t']) / 60 / 8:
+                        if mapSplitData[i + 1]['b'] - mapSplitData[i]['b'] >= 0.25:
                             direction = reverseCutDirection(direction)
                 else:  # Can't find anything that could help, just going to ignore that pattern
                     continue
@@ -338,8 +338,7 @@ def flowDetector(mapSplitData: list, bombData: list, leftOrRight):
         mapSplitData[i]['head'] = False
     # Find the first note
     if mapSplitData[0]['d'] == 8:
-        if mapSplitData[1]['d'] != 8 and mapSplitData[1]['b'] - mapSplitData[0]['b'] <= findBPM(mapSplitData[0]['t']) \
-                / 60 / 16:
+        if mapSplitData[1]['d'] != 8 and mapSplitData[1]['b'] - mapSplitData[0]['b'] <= 0.125:
             mapSplitData[0]['dir'] = mod(cut_direction_index[mapSplitData[1]['d']] + mapSplitData[1]['a'], 360)
         else:
             # Use the first arrow found to reverse search the direction
@@ -348,7 +347,7 @@ def flowDetector(mapSplitData: list, bombData: list, leftOrRight):
                 found = tempList[0]
                 foundAngle = mod(cut_direction_index[found['d']] + found['a'], 360)
                 for i in range(mapSplitData.index(found), 0, -1):
-                    if mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] >= findBPM(mapSplitData[i]['t']) / 60 / 8:
+                    if mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] >= 0.25:
                         mapSplitData[0]['dir'] = reverseCutDirection(foundAngle)
             elif mapSplitData[0]['y'] >= 2:  # Otherwise, use position instead
                 mapSplitData[0]['dir'] = 90  # Assume up for top row
@@ -359,9 +358,9 @@ def flowDetector(mapSplitData: list, bombData: list, leftOrRight):
     # Find the second note
     if mapSplitData[1]['d'] == 8:
         # Pattern?
-        if (mapSplitData[1]['b'] - mapSplitData[0]['b'] <= findBPM(mapSplitData[0]['t']) / 60 / 8
+        if (mapSplitData[1]['b'] - mapSplitData[0]['b'] <= 0.25
             and isSlider(mapSplitData[0], mapSplitData[1], mapSplitData[0]['dir'], True)) \
-                or mapSplitData[1]['b'] - mapSplitData[0]['b'] <= findBPM(mapSplitData[0]['t']) / 60 / 16:
+                or mapSplitData[1]['b'] - mapSplitData[0]['b'] <= 0.125:
             mapSplitData[1]['dir'] = findAngleViaPosition(mapSplitData, 1, 0, mapSplitData[0]['dir'], True)
             if mapSplitData[0]['d'] == 8:
                 mapSplitData[0]['dir'] = mapSplitData[1]['dir']
@@ -386,9 +385,9 @@ def flowDetector(mapSplitData: list, bombData: list, leftOrRight):
             else:
                 mapSplitData[1]['dir'] = findAngleViaPosition(mapSplitData, 1, 0, mapSplitData[0]['dir'], False)
     else:
-        if (mapSplitData[1]['b'] - mapSplitData[0]['b'] <= findBPM(mapSplitData[0]['t']) / 60 / 8
+        if (mapSplitData[1]['b'] - mapSplitData[0]['b'] <= 0.25
             and isSlider(mapSplitData[0], mapSplitData[1], mapSplitData[0]['dir'], False)) \
-                or mapSplitData[1]['b'] - mapSplitData[0]['b'] <= findBPM(mapSplitData[0]['t']) / 60 / 16:
+                or mapSplitData[1]['b'] - mapSplitData[0]['b'] <= 0.125:
             mapSplitData[0]['head'] = True
             mapSplitData[0]['pattern'] = True
             mapSplitData[1]['pattern'] = True
@@ -397,9 +396,9 @@ def flowDetector(mapSplitData: list, bombData: list, leftOrRight):
     for i in range(2, len(mapSplitData) - 1):
         if mapSplitData[i]['d'] == 8:  # Dot note
             # If under 0.25 and placement matches, probably a pattern
-            if (mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] <= findBPM(mapSplitData[i]['t']) / 60 / 8
+            if (mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] <= 0.25
                 and isSlider(mapSplitData[i - 1], mapSplitData[i], mapSplitData[i - 1]['dir'], True)) \
-                    or mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] <= findBPM(mapSplitData[i]['t']) / 60 / 16:
+                    or mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] <= 0.125:
                 mapSplitData[i]['dir'] = findAngleViaPosition(mapSplitData, i, i - 1, mapSplitData[i - 1]['dir'], True)
                 if mapSplitData[i - 1]['d'] == 8:
                     mapSplitData[i - 1]['dir'] = mapSplitData[i]['dir']
@@ -463,9 +462,9 @@ def flowDetector(mapSplitData: list, bombData: list, leftOrRight):
                     mapSplitData[i]['dir'] = mod(mapSplitData[i] - testValue * 2, 360)
         else:  # Arrow note
             mapSplitData[i]['dir'] = mod(cut_direction_index[mapSplitData[i]['d']] + mapSplitData[i]['a'], 360)
-            if (mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] <= findBPM(mapSplitData[i]['t']) / 60 / 8
+            if (mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] <= 0.25
                 and isSlider(mapSplitData[i - 1], mapSplitData[i], mapSplitData[i - 1]['dir'], False)) \
-                    or mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] <= findBPM(mapSplitData[i]['t']) / 60 / 16:
+                    or mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] <= 0.125:
                 mapSplitData[i]['pattern'] = True
                 mapSplitData[i]['bomb'] = mapSplitData[i - 1]['bomb']
                 if mapSplitData[i - 1]['pattern'] is False:
@@ -478,8 +477,7 @@ def flowDetector(mapSplitData: list, bombData: list, leftOrRight):
                 continue
     for i in range(2, len(mapSplitData) - 2):
         # Not a pattern and the note parity only work from before or after
-        if mapSplitData[i]['d'] == 8 and mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] >= \
-                findBPM(mapSplitData[i]['t']) / 60 / 16:
+        if mapSplitData[i]['d'] == 8 and mapSplitData[i]['b'] - mapSplitData[i - 1]['b'] >= 0.125:
             if (isSameDirection(mapSplitData[i]['dir'], mapSplitData[i - 1]['dir']) is True and mapSplitData[i]['bomb']
                 is False and isSameDirection(mapSplitData[i]['dir'], mapSplitData[i + 1]['dir']) is False) or \
                     ((isSameDirection(mapSplitData[i]['dir'], mapSplitData[i - 1]['dir']) is False and
@@ -495,9 +493,9 @@ def flowDetector(mapSplitData: list, bombData: list, leftOrRight):
     # Handle the last note
     if mapSplitData[-1]['d'] == 8:
         # Pattern?
-        if (mapSplitData[-1]['b'] - mapSplitData[-2]['b'] <= findBPM(mapSplitData[-1]['t']) / 60 / 8
+        if (mapSplitData[-1]['b'] - mapSplitData[-2]['b'] <= 0.25
             and isSlider(mapSplitData[-2], mapSplitData[-1], mapSplitData[-2]['dir'], True)) \
-                or mapSplitData[-1]['b'] - mapSplitData[-2]['b'] <= findBPM(mapSplitData[-1]['t']) / 60 / 16:
+                or mapSplitData[-1]['b'] - mapSplitData[-2]['b'] <= 0.125:
             mapSplitData[-1]['dir'] = findAngleViaPosition(mapSplitData, len(mapSplitData) - 1,
                                                            len(mapSplitData) - 2, mapSplitData[0]['dir'], True)
             if mapSplitData[-2]['d'] == 8:
@@ -526,9 +524,9 @@ def flowDetector(mapSplitData: list, bombData: list, leftOrRight):
                 mapSplitData[-1]['dir'] = reverseCutDirection(mapSplitData[-2]['dir'])
     else:
         mapSplitData[-1]['dir'] = mod(cut_direction_index[mapSplitData[-1]['d']] + mapSplitData[-1]['a'], 360)
-        if (mapSplitData[-1]['b'] - mapSplitData[-2]['b'] <= findBPM(mapSplitData[-1]['t']) / 60 / 8
+        if (mapSplitData[-1]['b'] - mapSplitData[-2]['b'] <= 0.25
             and isSlider(mapSplitData[-2], mapSplitData[-1], mapSplitData[-2]['dir'], False)) \
-                or mapSplitData[-1]['b'] - mapSplitData[-2]['b'] <= findBPM(mapSplitData[-1]['t']) / 60 / 16:
+                or mapSplitData[-1]['b'] - mapSplitData[-2]['b'] <= 0.125:
             mapSplitData[-1]['pattern'] = True
             mapSplitData[-1]['bomb'] = mapSplitData[-2]['bomb']
             if mapSplitData[-2]['pattern'] is False:
